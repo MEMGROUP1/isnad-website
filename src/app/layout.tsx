bfeb9d3181fe549dd7d2284f09e04b584a4aacce
MEMGROUP1 <shahad.modher@mem-iq.com>
2026-05-30 17:35:29 +0300

    Integrate Google Analytics in RootLayout
    Added Google Analytics scripts to the root layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,28 @@
 // and must not render any UI except for the children.
 // See: https://next-intl-docs.vercel.app/docs/getting-started/app-router
 
+import Script from "next/script";
+
 export default function RootLayout({
-    children,
-}: Readonly<{
-    children: React.ReactNode;
-}>) {
-    return children;
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-KCETV292NZ"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-KCETV292NZ');
+        `}
+      </Script>
+      {children}
+    </>
+  );
 }
