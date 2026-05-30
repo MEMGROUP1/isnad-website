90ae0bb897fca696697cb63be8e02865edb243d0
MEMGROUP1 <shahad.modher@mem-iq.com>
2026-05-30 17:44:42 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,28 +2,10 @@
 // and must not render any UI except for the children.
 // See: https://next-intl-docs.vercel.app/docs/getting-started/app-router
 
-import Script from "next/script";
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <>
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-KCETV292NZ"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-KCETV292NZ');
-        `}
-      </Script>
-      {children}
-    </>
-  );
+  return children;
 }
